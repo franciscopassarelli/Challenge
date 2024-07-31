50eace6df465f04da647311a8be9e792d838c4bc
francisco passarelli <118229728+franciscopassarelli@users.noreply.github.com>
2024-07-31 04:39:30 -0300

    tercer

--- my-challenge/components/Navbar.js
+++ my-challenge/components/Navbar.js
@@ -1,7 +1,6 @@
 // components/Navbar.js
 
 import styles from './Navbar.module.css'
-import Image from 'next/image'
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
