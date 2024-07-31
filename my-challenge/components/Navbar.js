f44285240f6a75eb03fbc76cb4584e8bcbefb661
francisco passarelli <118229728+franciscopassarelli@users.noreply.github.com>
2024-07-31 04:02:53 -0300

    segundocommit

--- my-challenge/components/Navbar.js
+++ my-challenge/components/Navbar.js
@@ -1,12 +1,12 @@
 // components/Navbar.js
 
 import styles from './Navbar.module.css'
-
+import Image from 'next/image'
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
-        <img src="https://bytebucket.org/estoes-challenges/frontend/raw/30458c8234b0a017ad65869dafb74abea437b0ea/assets/images/logo.png" alt="Logo" />
+        <Image src="https://bytebucket.org/estoes-challenges/frontend/raw/30458c8234b0a017ad65869dafb74abea437b0ea/assets/images/logo.png" alt="Logo" />
       </div>
     </nav>
   )
